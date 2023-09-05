6f28511c573371edb38799690983762ea72ff0fa
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-09-05 21:32:55 +0200

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
+
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
 
 function App() {
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+
+  function handleChange(event) {
+    setMarkdown(event.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="editorBox">
+        <h2>Editor</h2>
+        <textarea
+          id="editor"
+          onChange={handleChange}
+          value={markdown}
+        ></textarea>
+      </div>
+      <div>
+        <h2>Preview</h2>
+        <div id="preview">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+      </div>
     </div>
   );
 }
